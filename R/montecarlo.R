356760e8e7aefab1c87e21096fc6c8cde8a8aa41
James Scott <jgscott@gmail.com>
2015-07-31 09:46:12 -0500

    portfolio scripts

---
+++ R/montecarlo.R
@@ -0,0 +1,113 @@
+library(foreach)  # I typically use this for MC simulations
+
+# First, a riskless asset
+Horizon = 40
+ReturnAvg = 0.05
+
+Wealth = 10000
+# Sweep through each year and update the value of wealth
+for(year in 1:Horizon) {
+	ThisReturn = ReturnAvg
+	Wealth = Wealth * (1 + ThisReturn)
+}
+Wealth
+
+# The usual compound-interest formula
+10000 * (1+ReturnAvg)^40
+
+
+# Same thing, except now we track wealth over time
+
+Wealth = 10000  # Reset initial wealth
+WealthOverTime = rep(0, Horizon)  # Allocate some space
+# Sweep through each year and update the value of wealth
+for(year in 1:Horizon) {
+  ThisReturn = ReturnAvg
+  Wealth = Wealth * (1 + ThisReturn)
+  WealthOverTime[year] = Wealth
+}
+Wealth
+plot(WealthOverTime)
+
+
+
+# Now a risky asset with a positive expected return
+ReturnAvg = 0.05
+ReturnSD = 0.05
+Horizon = 40
+
+Wealth = 10000  # Reset initial wealth
+WealthOverTime = rep(0, Horizon)  # Allocate some space
+# Sweep through each year and update the value of wealth
+for(year in 1:Horizon) {
+  ThisReturn = rnorm(1, ReturnAvg, ReturnSD)
+  Wealth = Wealth * (1 + ThisReturn)
+  WealthOverTime[year] = Wealth
+}
+Wealth
+plot(WealthOverTime)
+
+
+# Now a Monte Carlo simulation
+ReturnAvg = 0.05
+ReturnSD = 0.05
+Horizon = 40
+sim1 = foreach(i=1:1000, .combine='c') %do% {
+	Wealth = 10000  # Reset initial wealth
+	# Sweep through each year and update the value of wealth
+	for(year in 1:Horizon) {
+		# Generate a random return
+		ThisReturn = rnorm(1, ReturnAvg, ReturnSD)
+		
+		# Update wealth
+		Wealth = Wealth * (1 + ThisReturn)
+	}
+	# Output the value of wealth for each simulated scenario
+	Wealth
+}
+hist(sim1, 50)
+abline(v=10000 * (1+ReturnAvg)^40, col='red', lwd=3)
+
+# We can easily calculate expected value from the MC simulation.
+# Just take a simple average of the simulated futures.
+mean(sim1)
+sd(sim1)
+
+#  We can also save each year's result in each simulated scenario
+ReturnAvg = 0.05
+ReturnSD = 0.025
+Horizon = 40
+sim1 =  = 40
+sim1 = foreach(i=1:500, .combine='rbind') %do% {
+  Wealth = 10000  # Reset initial wealth
+  WealthOverTime = rep(0, Horizon)  # Allocate some space
+  # Sweep through each year and update the value of wealth
+  for(year in 1:Horizon) {
+    ThisReturn = rnorm(1, ReturnAvg, ReturnSD)
+    Wealth = Wealth * (1 + ThisReturn)
+    WealthOverTime[year] = Wealth
+  }
+  WealthOverTime
+}
+head(sim1)
+
+# Plot a few simulated scenarios
+plot(1:Horizon, sim1[1,], type='l')
+lines(1:Horizon, sim1[2,], type='l')
+lines(1:Horizon, sim1[3,], type='l')
+
+# A spaghetti plot to show variability over time.
+# type ?par into the console to see lots of graphical settings
+plot(1:Horizon, colMeans(sim1), type='l', col='red',
+	las=1, xlab='Year', ylab='Value',
+	main='A 40-year portfolio: uncertainty over time', cex.axis=0.8)
+for(sim in 1:nrow(sim1)) {
+  lines(1:Horizon, sim1[sim,], type='l', col=rgb(0,0,0.5,0.05))
+}
+lines(1:Horizon, colMeans(sim1), col='red', lwd=2)
+
+
+# Now look at terminal wealth (the last column)
+hist(sim1[,Horizon])
+mean(sim1[, Horizon])
+sd(sim1[, Horizon])
---
+++ R/portfolio.R
@@ -0,0 +1,138 @@
+library(mosaic)
+library(fImport)
+library(foreach)
+
+# Import a few stocks
+mystocks = c("MRK", "JNJ", "SPY")
+myprices = yahooSeries(mystocks, from='2011-01-01', to='2015-07-30')
+# The first few rows
+head(myprices)
+
+
+# A helper function for calculating percent returns from a Yahoo Series
+# Source this to the console first, and then it will be available to use
+# (Like importing a library)
+YahooPricesToReturns = function(series) {
+	mycols = grep('Adj.Close', colnames(series))
+	closingprice = series[,mycols]
+	N = nrow(closingprice)
+	percentreturn = as.data.frame(closingprice[2:N,]) / as.data.frame(closingprice[1:(N-1),]) - 1
+	mynames = strsplit(colnames(percentreturn), '.', fixed=TRUE)
+	mynames = lapply(mynames, function(x) return(paste0(x[1], ".PctReturn")))
+	colnames(percentreturn) = mynames
+	as.matrix(na.omit(percentreturn))
+}
+
+
+# Compute the returns from the closing prices
+myreturns = YahooPricesToReturns(myprices)
+
+# These returns can be viewed as draws from the joint distribution
+pairs(myreturns)
+plot(myreturns[,1], type='l')
+
+# Look at the market returns over time
+plot(myreturns[,3], type='l')
+
+# An autocorrelation plot: nothing there
+acf(myreturns[,3])
+
+# The sample correlation matrix
+cor(myreturns)
+
+#######
+# A classical way to estimate portfolio variability: use the CAPM
+#######
+
+# First fit the market model to each stock
+lm_MRK = lm(myreturns[,1] ~ myreturns[,4])
+lm_JNJ = lm(myreturns[,2] ~ myreturns[,4])
+
+# The estimated beta for each stock based on daily returns
+coef(lm_MRK); coef(lm_JNJ)
+
+
+# A problem...
+# What about the residuals?
+myresiduals = cbind(resid(lm_ORCL), resid(lm_MRK), resid(lm_JNJ))
+
+# Sample correlation matrix
+cor(myresiduals)
+
+# Compute the moments of a one-day change in your portfolio
+totalwealth = 10000
+weights = c(0.5, 0.25, 0.25) 	# What percentage of your wealth will you put in each stock?
+
+mu_SPY = mean(myreturns[,4])
+sigma_SPY = sd(myreturns[,4])
+
+
+
+# How much money do we have in each stock?
+holdings = weights * totalwealth
+
+
+#### Now use a bootstrap approach
+#### With more stocks
+
+mystocks = c("WMT", "TGT", "XOM", "MRK", "JNJ")
+myprices = yahooSeries(mystocks, from='2011-01-01', to='2015-07-30')
+
+# Compute the returns from the closing prices
+myreturns = YahooPricesToReturns(myprices)
+pairs(myreturns)
+
+# Sample a random return from the empirical joint distribution
+# This simulates a random day
+return.today = resample(myreturns, 1, orig.ids=FALSE)
+
+# Update the value of your holdings
+holdings = holdings + holdings*return.today
+
+# Compute your new total wealth
+totalwealth = sum(holdings)
+
+
+# Now loop over two trading weeks
+totalwealth = 10000
+weights = c(0.2, 0.2, 0.2, 0.2, 0.2)
+holdings = weights * totalwealth
+n_days = 10
+wealthtracker = rep(0, n_days) # Set up a placeholder to track total wealth
+for(today in 1:n_days) {
+	return.today = resample(myreturns, 1, orig.ids=FALSE)
+	holdings = holdings + holdings*return.today
+	totalwealth = sum(holdings)
+	wealthtracker[today] = totalwealth
+}
+totalwealth
+plot(wealthtracker, type='l')
+
+
+# Now simulate many different possible trading years!
+sim1 = foreach(i=1:500, .combine='rbind') %do% {
+	totalwealth = 10000
+	weights = c(0.2, 0.2, 0.2, 0.2, 0.2)
+	holdings = weights * totalwealth
+	wealthtracker = rep(0, n_days) # Set up a placeholder to track total wealth
+	for(today in 1:n_days) {
+		return.today = resample(myreturns, 1, orig.ids=FALSE)
+		holdings = holdings + holdings*return.today
+		totalwealth = sum(holdings)
+		wealthtracker[today] = totalwealth
+	}
+	wealthtracker
+}
+
+head(sim1)
+hist(sim1[,n_days])
+
+# Profit/loss
+hist(sim1[,n_days]- 10000)
+
+# Calculate 5% value at risk
+quantile(sim1[,n_days], 0.05) - 10000
+
+
+
+
